420621935b8f1a5ee7d237b7af7634b2d453ddb6
Gary Mirams <gary.mirams@gmail.com>
2014-08-27 18:20:44 +0000

    [Frontiers] #2558 some initial timing results
    git-svn-id: file:///var/svn/chaste/projects/Frontiers2014@22511 b5f69653-2bf5-0310-8201-b0dafb8e1405

---
+++ test/results/put_underscores_in_latex.m
@@ -0,0 +1,7 @@
+function modifiedStr = put_underscores_in_latex( input_string )
+%PUT_UNDERSCORES_IN_LATEX Summary of this function goes here
+%   Detailed explanation goes here
+modifiedStr = strrep(input_string, '_', '\_');
+
+end
+
